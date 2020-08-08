10e65cafb1815c44556e796165d3dbf9d99db4b4
Curtis Rose <ccr46@nau.edu>
2020-08-07 17:59:53 -0700

    Inititial Release

---
+++ Assets/Scripts/CelestialBody.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CelestialBody : MonoBehaviour
+{
+    [SerializeField]
+    protected Transform body;
+    [SerializeField]
+    protected float mass; // Kg
+    [SerializeField]
+    protected float diameter; // Km
+
+    [SerializeField]
+    protected double revolutionPeriod; // days
+
+    SolarSystem solarSystem;
+
+    Rigidbody rb;
+
+    Vector3 force = new Vector3(0f, 0f, 0f);
+
+    private void Awake()
+    {
+        rb = gameObject.GetComponentInChildren<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rb.useGravity = false;
+
+        solarSystem = GetComponentInParent<SolarSystem>();
+        solarSystem.AddCelestialBody(this);
+
+        //rb.velocity = startingVelocity * (new Vector3(Random.Range(-1f, 1f), Random.Range(-0.5f, 0.5f), Random.Range(-1f, 1f)));
+        //rb.angularVelocity = startingVelocity * (new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)));
+        //SetSize(mass);
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyForces();
+        force = new Vector3(0f, 0f, 0f);
+    }
+
+    public void ApplyForces()
+    {
+        rb.AddForce(force, ForceMode.Acceleration);
+    }
+
+    public void AddForce(Vector3 force)
+    {
+        this.force += force;
+    }
+
+    public float GetMass()
+    {
+        return (float)mass;
+    }
+
+    public void SetSize(float mass)
+    {
+        this.mass = mass;
+        diameter = 2 * Mathf.Pow((3f / 4f) * Mathf.PI * mass, 1f / 3f);
+        body.localScale = Vector3.one * diameter / 2.66134f;
+    }
+
+    public Rigidbody GetRigidBody()
+    {
+        return rb;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    public void Die()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/SolarSystem.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SolarSystem : MonoBehaviour
+{
+    [SerializeField]
+    Transform celestialBodyPrefab;
+
+    private List<CelestialBody> celestialBodies = new List<CelestialBody>();
+
+    public int numCelestialBodies = 0;
+
+    public float startingTranslationalVelocity = 0;
+    public float startingRotationalVelocity = 0;
+
+    public float startingPositionScalar = 0;
+
+    public float gravityScalar = 1;
+
+    public List<CelestialBody> GetCelestialBodies()
+    {
+        return celestialBodies;
+    }
+
+    public Vector3 GetGravitationalVector(Vector3 position)
+    {
+        return Vector3.one;
+    }
+
+    public void AddCelestialBody(CelestialBody celestialBody)
+    {
+        celestialBodies.Add(celestialBody);
+    }
+
+    private void Start()
+    {
+        for (int i = 0; i < numCelestialBodies; i++)
+        {
+            CelestialBody celestialBody = 
+                Instantiate(celestialBodyPrefab, this.transform).GetComponent<CelestialBody>();
+            celestialBody.SetSize(1);
+            Rigidbody rb = celestialBody.GetRigidBody();
+            rb.useGravity = false;
+            rb.velocity = startingTranslationalVelocity * (new Vector3(Random.Range(-1f, 1f), Random.Range(-0.5f, 0.5f), Random.Range(-1f, 1f)));
+            rb.angularVelocity = startingRotationalVelocity * (new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)));
+            celestialBody.transform.position = startingPositionScalar * new Vector3(Random.Range(-1f,1f), Random.Range(-0.1f, 0.1f), Random.Range(-1f, 1f));
+        }
+    }
+
+    void FixedUpdate()
+    {
+        for (int i = 0; i < celestialBodies.Count; i++)
+        {
+            for (int j = i + 1; j < celestialBodies.Count; j++)
+            {
+                Vector3 forceVector = new Vector3(0, 0, 0);
+                float distance = 0;
+                forceVector = celestialBodies[j].transform.position - celestialBodies[i].transform.position;
+                distance = forceVector.magnitude;
+                if (distance > 1)
+                {
+                    float forceMagnitude = gravityScalar * (celestialBodies[i].GetMass() * celestialBodies[j].GetMass()) / (distance * distance);
+                    celestialBodies[i].AddForce(forceVector.normalized * forceMagnitude);
+                    celestialBodies[j].AddForce(forceVector.normalized * forceMagnitude * -1);
+                }
+            }
+        }
+    }
+
+    public void Merge()
+    {
+       
+    }
+}
