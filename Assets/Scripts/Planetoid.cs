a949b994557fa5d40171c7fb3e50c12a8fdbdb5d
Curtis Rose <ccr46@nau.edu>
2020-08-12 20:47:52 -0700

    Collision

--- Assets/Scripts/Planetoid.cs
+++ Assets/Scripts/Planetoid.cs
@@ -12,8 +12,9 @@ public class Planetoid : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        startingVelocity = new Vector3(0f, 0f, 0f);
-        rb = GetComponent<Rigidbody>();
-        rb.velocity = startingVelocity;
+        foreach(CelestialBody celestialBody in GetComponentsInChildren<CelestialBody>())
+        {
+            celestialBody.AddForce(startingVelocity);
+        }
     }
 }
